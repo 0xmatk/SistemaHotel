110966a4ce5b26833d0e9eacdbfeffa9869a6a94
0xMatk <79279085+0xmatk@users.noreply.github.com>
2024-05-31 12:25:12 -0300

    se agrega clase reserva

--- src/Clases/Estadia.java
+++ src/Clases/Estadia.java
@@ -2,59 +2,13 @@ package Clases;
 
 import java.util.Date;
 
-public class Estadia {
-    private Visitante visitante;
-    private Habitacion habitacion;
-    private Date checkIn;
-    private Date checkOut;
-
-    public Estadia(Visitante visitante, Date checkOut, Habitacion habitacion, Date checkIn) {
-        this.visitante = visitante;
-        this.checkOut = checkOut;
-        this.habitacion = habitacion;
-        this.checkIn = checkIn;
-    }
+public class Estadia extends Reserva{
+    private boolean checkIn;
+    private boolean checkOut;
 
 
-    public Visitante getVisitante() {
-        return visitante;
-    }
+    public Estadia(Visitante visitante, Habitacion habitacion, Date llegada, Date salida) {
+        super(visitante, habitacion, llegada, salida);
 
-    public void setVisitante(Visitante visitante) {
-        this.visitante = visitante;
-    }
-
-    public Date getCheckOut() {
-        return checkOut;
-    }
-
-    public void setCheckOut(Date checkOut) {
-        this.checkOut = checkOut;
-    }
-
-    public Date getCheckIn() {
-        return checkIn;
-    }
-
-    public void setCheckIn(Date checkIn) {
-        this.checkIn = checkIn;
-    }
-
-    public Habitacion getHabitacion() {
-        return habitacion;
-    }
-
-    public void setHabitacion(Habitacion habitacion) {
-        this.habitacion = habitacion;
-    }
-
-    @Override
-    public String toString() {
-        return "Clases.Estadia{" +
-                "visitante=" + visitante +
-                ", habitacion=" + habitacion +
-                ", checkIn=" + checkIn +
-                ", checkOut=" + checkOut +
-                '}';
     }
 }
--- src/Clases/Hotel.java
+++ src/Clases/Hotel.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Hotel {
     List<Habitacion> habitaciones;
+    List<Empleado> empleados;
+    List<Estadia> estadias;
 
 
 
