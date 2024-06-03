120d2eeefd7a4a9b490132804d33247208868a69
Matias <matiasklingeman114@outlook.com>
2024-06-03 17:59:13 -0300

    se genera herencia de Estadia y reserva con validacion de habitacion disponible y costo de estadia

--- src/Clases/Reserva.java
+++ src/Clases/Reserva.java
@@ -3,17 +3,62 @@ package Clases;
 import java.util.Date;
 
 public class Reserva {
-    private Visitante visitante;
-    private Habitacion habitacion;
-    private Date llegada;
-    private Date salida;
+    protected Visitante visitante;
+    protected Habitacion habitacion;
+    protected Date llegada;
+    protected Date salida;
 
     public Reserva(Visitante visitante, Habitacion habitacion, Date llegada, Date salida) {
-        this.visitante = visitante;
-        this.habitacion = habitacion;
-        this.llegada = llegada;
-        this.salida = salida;
+        if(habitacion.isDisponible()){
+            this.visitante = visitante;
+            this.habitacion = habitacion;
+            this.llegada = llegada;
+            this.salida = salida;
+        }else{
+            System.out.println("No se puede realizar una reserva. Habitacion agotada.");
+        }
     }
 
 
+    public Visitante getVisitante() {
+        return visitante;
+    }
+
+    public void setVisitante(Visitante visitante) {
+        this.visitante = visitante;
+    }
+
+    public Date getSalida() {
+        return salida;
+    }
+
+    public void setSalida(Date salida) {
+        this.salida = salida;
+    }
+
+    public Date getLlegada() {
+        return llegada;
+    }
+
+    public void setLlegada(Date llegada) {
+        this.llegada = llegada;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "visitante=" + visitante +
+                ", habitacion=" + habitacion +
+                ", llegada=" + llegada +
+                ", salida=" + salida +
+                '}';
+    }
 }
