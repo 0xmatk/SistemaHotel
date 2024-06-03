4b52240921dcef4e5ec5c2fa658226eb86c62cba
0xMatk <79279085+0xmatk@users.noreply.github.com>
2024-06-03 17:59:13 -0300

    se genera herencia de Estadia y reserva con validacion de habitacion disponible y costo de estadia

--- src/Clases/Estadia.java
+++ src/Clases/Estadia.java
@@ -1,14 +1,31 @@
 package Clases;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 
 public class Estadia extends Reserva{
     private boolean checkIn;
     private boolean checkOut;
+    private int costo;
 
 
     public Estadia(Visitante visitante, Habitacion habitacion, Date llegada, Date salida) {
         super(visitante, habitacion, llegada, salida);
+        this.costo = calcularCosto();
+
+    }
+
+    public int calcularCosto(){
+      int costoTotal = 0;
+
+      long diferenciaMilisegundos = salida.getTime() - llegada.getTime();
+
+      long diferenciaDias = TimeUnit.DAYS.convert(diferenciaMilisegundos, TimeUnit.MILLISECONDS);
+
+        costoTotal = (int)diferenciaDias * habitacion.getCoste();
+
+        return costoTotal;
 
     }
 }
