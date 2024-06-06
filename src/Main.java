2913add885332eaa62918bc7c2a09c9c1c137f9a
Matias <matiasklingeman114@outlook.com>
2024-06-06 20:06:59 -0300

    testeo main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import Clases.Habitacion;
+import Clases.Hotel;
+import Clases.Reserva;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+        Habitacion habitacion = new Habitacion("tipo", true, 1, 1);
+
+        Reserva reserva = new Reserva(null, habitacion, null, null);
+        hotel.agregarReserva(reserva);
+
+
+
   }
 
 }
