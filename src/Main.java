cfde0f5b744c60342166397698d1c99aed744c56
Matias <matiasklingeman114@outlook.com>
2024-06-06 20:44:50 -0300

    se agregan funciones para manipular lista de datos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import Clases.Habitacion;
-import Clases.Hotel;
-import Clases.Reserva;
+import Clases.*;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,9 +8,20 @@ public class Main {
     public static void main(String[] args) {
         Hotel hotel = new Hotel();
         Habitacion habitacion = new Habitacion("tipo", true, 1, 1);
+        Date llegada = new Date();
+        Date salida = new Date();
 
-        Reserva reserva = new Reserva(null, habitacion, null, null);
-        hotel.agregarReserva(reserva);
+        Reserva reserva = new Reserva(null, null, null, null);
+        Empleado empleado = new Empleado("Matias", "Klingeman", 1, hotel);
+        Visitante visitante = new Visitante("Jose", "Perez", 1213, "Mar del Plata", "Calle123", true);
+
+        empleado.crearReserva(visitante, habitacion, llegada, salida, true);
+        empleado.crearReserva(visitante, habitacion, llegada, salida, true);
+
+        hotel.mostrarReservas();
+
+
+
 
 
 
